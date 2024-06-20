487352a0a662a10ed42e165008096e07b7a7f9ac
RODRIGO IGNACIO LÓPEZ SALGADO <52356618+Therodrogo@users.noreply.github.com>
2024-06-19 21:09:10 -0400

    Validacion de QR
    Se valida que tenga el formato "Mesa 1 CucchoGrill"

--- Frontend/src/components/QRScan2.jsx
+++ Frontend/src/components/QRScan2.jsx
@@ -8,7 +8,7 @@ const QRCodeScanner = () => {
     const canvasRef = useRef(null);
     const [scanner, setScanner] = useState(null);
 
-    const [camaraActiva,setCamaraActiva] = useState(false)
+    const [orden, setOrden] = useState("")
 
     useEffect(() => {
         if (scanner) {
@@ -25,15 +25,50 @@ const QRCodeScanner = () => {
     useEffect(() => {
         encenderCamara()
     }, [])
-    
+
 
     const encenderCamara = () => {
         const qrScanner = new QrScanner(
             videoRef.current,
             result => {
                 Swal.fire('QR Code', `Contenido: ${result}`, 'success');
-                setScanner(null)
-            
+
+                const match = result.match(/^(\D+)\s(\d+)\s(.+)$/);
+
+                if (match) {
+                    const primeraParte = match[1].trim(); // "Mesa"
+                    const numero = match[2]; // "1"
+                    const segundaParte = match[3]; // "CucchoGrill"
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Escanero con exito",
+                        showConfirmButton: false,
+                        timer: 1500,
+                        customClass: {
+                            popup: 'swal2-popup',
+                            title: 'swal2-title',
+                        }
+                    });
+                    setScanner(null)
+
+
+                } else {
+                    Swal.fire({
+                        position: "center",
+                        icon: "error",
+                        title: "QR incorrecto",
+                        showConfirmButton: false,
+                        timer: 1500,
+                        customClass: {
+                            popup: 'swal2-popup',
+                            title: 'swal2-title',
+                        }
+                    });
+                }
+
+              
+
             },
             error => {
                 console.error(error);
@@ -51,16 +86,12 @@ const QRCodeScanner = () => {
 
     return (
         <div>
-            
-                <h5 className="text-center">Escanear código QR</h5>
-                <Card style={{marginLeft:"20px",marginRight:"20px"}}> 
-                    <CardBody>
-
-                        <video style={{borderRadius:"12px"}} ref={videoRef}></video>
-
-                    </CardBody>
-                </Card>
-
+            <h5 className="text-center">Escanear código QR</h5>
+            <Card style={{ marginLeft: "20px", marginRight: "20px" }}>
+                <CardBody>
+                    <video style={{ borderRadius: "12px" }} ref={videoRef}></video>
+                </CardBody>
+            </Card>
         </div>
     );
 };
